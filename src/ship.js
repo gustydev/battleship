7f4901cda48a9472243477ddfae274cd4cd51479
gusty <gustavodiorlando@gmail.com>
2024-02-28 11:35:49 -0300

    Write isSunk function + tests for ship

--- src/ship.js
+++ src/ship.js
@@ -4,6 +4,17 @@ class Ship {
         this.timesHit = 0;
         this.sunk = false;
     }
+    hit() {
+        if (this.timesHit < this.length) {
+            this.timesHit += 1;
+        }
+        if (this.timesHit === this.length) {
+            this.sunk = true;
+        }
+    }
+    isSunk() {
+        return this.sunk;
+    }
 }
 
 module.exports = Ship;
--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -1,1 +1,18 @@
-const ship = require('../ship');
+const Ship = require('../ship');
+
+test('testing hit function', () => {
+    const shipThree = new Ship(3);
+    shipThree.hit();
+    expect(shipThree.timesHit).toBe(1);
+    for (let i = 0; i < 100; i++) {
+        shipThree.hit();
+    }
+    expect(shipThree.timesHit).toBe(3);
+})
+
+test('testing isSunk function', () => {
+    const shipOne = new Ship(1);
+    expect(shipOne.isSunk()).toBeFalsy();
+    shipOne.hit();
+    expect(shipOne.isSunk()).toBeTruthy();
+})
