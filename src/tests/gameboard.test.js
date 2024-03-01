26efd184193eb911e408aaeccc2e87b0492f51db
gusty <gustavodiorlando@gmail.com>
2024-03-01 15:29:15 -0300

    Fix bugs with ship class positions array

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -3,10 +3,10 @@ const Ship = require('../ship');
 
 test('grid test', () => {
     const testBoard = new Gameboard();
-    const a1 = {coord: 'A1', hit: false}
-    const f8 = {coord: 'F8', hit: false}
-    const j10 = {coord: 'J10', hit: false}
-    const z99 = {coord: 'Z99', hit: false}
+    const a1 = {coord: 'A1', ship: undefined, hit: false}
+    const f8 = {coord: 'F8', ship: undefined, hit: false}
+    const j10 = {coord: 'J10', ship: undefined, hit: false}
+    const z99 = {coord: 'Z99', ship: undefined, hit: false}
     expect(testBoard.grid).toContainEqual(a1);
     expect(testBoard.grid).toContainEqual(f8);
     expect(testBoard.grid).toContainEqual(j10);
