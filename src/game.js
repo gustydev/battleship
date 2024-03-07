7315049be9bffbbac44edcf5bc550ffbe5bbd2b9
gusty <gustavodiorlando@gmail.com>
2024-03-07 12:03:07 -0300

    Style and position boards in page

--- src/game.js
+++ src/game.js
@@ -1,16 +1,12 @@
-const Gameboard = require('./gameboard');
 const Player = require('./player');
 const Ship = require('./ship');
 
-const computer = new Player('Computer');
-const human = new Player('Human');
-
-function game() {
-    computer.board.placeShip(new Ship('Carrier', 5), 'A1', 'horizontal')
-    computer.board.placeShip(new Ship('Battleship', 4), 'A2', 'horizontal')
-    computer.board.placeShip(new Ship('Cruiser', 3), 'A3', 'horizontal')
-    computer.board.placeShip(new Ship('Submarine', 3), 'A4', 'horizontal')
-    computer.board.placeShip(new Ship('Destroyer', 2), 'A5', 'horizontal')
+function game(human, comp) {
+    comp.board.placeShip(new Ship('Carrier', 5), 'A1', 'horizontal')
+    comp.board.placeShip(new Ship('Battleship', 4), 'A2', 'horizontal')
+    comp.board.placeShip(new Ship('Cruiser', 3), 'A3', 'horizontal')
+    comp.board.placeShip(new Ship('Submarine', 3), 'A4', 'horizontal')
+    comp.board.placeShip(new Ship('Destroyer', 2), 'A5', 'horizontal')
     
     human.board.placeShip(new Ship('Carrier', 5), 'A6', 'horizontal')
     human.board.placeShip(new Ship('Battleship', 4), 'A7', 'horizontal')
@@ -18,9 +14,9 @@ function game() {
     human.board.placeShip(new Ship('Submarine', 3), 'A9', 'horizontal')
     human.board.placeShip(new Ship('Destroyer', 2), 'A10', 'horizontal')
     // Later replace with ship placement function
-
-    while (!human.board.allSunk() && !computer.board.allSunk()) {
-        // game will run
+    
+    while (!human.board.allSunk() && !comp.board.allSunk()) {
+        // play game
     }
 
     if (human.board.allSunk()) {
